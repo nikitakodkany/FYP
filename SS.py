5f1cceb904d3beca6c42cbbddbdf939ab18c26e5
nikitakodkany <nikitahkodkany@gmail.com>
2021-09-16 22:09:21 +0530

    Quantified the values

--- SS.py
+++ SS.py
@@ -11,7 +11,7 @@ with open('/home/nikita/Desktop/PGN/Dataset/pros_Dataset2.csv', newline='') as f
 #To open KLSGIT syllabus
 with open('/home/nikita/Desktop/PGN/Dataset/syllabuskeys.csv', newline='') as f2:
     SylVtext2 = f2.readlines() #981
-    
+
 #A list that has the entire corpus
 EntireCorpus = JDVtext1 + SylVtext2
 
@@ -32,29 +32,33 @@ class WordIdGenerator:
 gen = WordIdGenerator()
 for w in EntireCorpus:
     wordtoindex = gen.word_id(w.split())
-    
+
 #intialising JD-values and Syllabus-values to zeros
 JDVhashed = np.zeros((6131,1400))
 SylVhashed = np.zeros((1400,1))
 
-#To have an encoded JD-Value vector where there are 6131 JDS and 1400 Word-To-Index Values 
+#To have an encoded JD-Value vector where there are 6131 JDS and 1400 Word-To-Index Values
 for i,sentence in enumerate(JDVtext1):
-    wordlist = sentence.split() 
+    wordlist = sentence.split()
     for word in wordlist:
         if word in wordtoindex:
             index = wordtoindex[word]
             JDVhashed[i][index] = 1
 
 #To have an encoded Syl-Value vector
-for sentence in SylVtext2: 
-    wordlist = sentence.split() 
+for sentence in SylVtext2:
+    wordlist = sentence.split()
     for word in wordlist:
         if word in wordtoindex:
             index = wordtoindex[word]
-            SylVhashed[index][0] = 1 
+            SylVhashed[index][0] = 1
 
 #Cosine Similarity Scores
 from sklearn.metrics.pairwise import cosine_similarity
-print("Similarity Score:", np.max((cosine_similarity(JDVhashed, SylVhashed.T))))
-print("Average Similarity Score: ", np.mean((cosine_similarity(JDVhashed, SylVhashed.T))))
-print("Dissimilarity Score:", 1-np.max((cosine_similarity(JDVhashed, SylVhashed.T))))
+score = np.max((cosine_similarity(JDVhashed, SylVhashed.T)))
+
+print("Similarity Score: {}" .format(score))
+print("Average Similarity Percentage: {}%" .format((np.mean(score))*100))
+print("Dissimilarity Percentage: {}%" .format((1-np.max(score))*100))
+
+print("The syllabus is {}% closer to the industry." .format(score*100))
